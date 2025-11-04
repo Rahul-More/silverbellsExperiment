8a58e0e55afa22ec2616e901a86ae9ffc71297e6
Rahul-More <rahul.more7861@gmail.com>
2025-11-04 20:49:01 +0530

    Update vite.config.ts with new configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// Set base path for GitHub Pages project site
+// https://vite.dev/guide/static-deploy.html#github-pages
 export default defineConfig({
+  base: '/silverbellsExperiment/',
   plugins: [react()],
   server: {
     port: 5173
